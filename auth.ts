be5e04e2202a32cef167f6ac97ff259e730d43aa
uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-11-09 23:42:03 +0100

    modified better-auth config

--- auth.ts
+++ auth.ts
@@ -18,6 +18,8 @@ export const auth = betterAuth({
     },
   },
 
+  baseURL: process.env.BETTER_AUTH_URL,
+
   hooks: {
     after: createAuthMiddleware(async (ctx) => {
       if (ctx.path === "/sign-up/email" || ctx.path === "/callback/:id") {
