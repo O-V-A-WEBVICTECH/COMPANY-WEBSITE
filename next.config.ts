713d4a6c5361b27c65fc4f036ace8967a6ece4f5
uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2026-06-05 05:30:20 +0100

    updated prisma config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/*": ["./generated/prisma/**/*"],
+    // Include the entire generated prisma folder for every routes
+    "**/*": ["./generated/prisma/**/*"],
   },
   serverExternalPackages: ["@prisma/client", "prisma"],
 };
