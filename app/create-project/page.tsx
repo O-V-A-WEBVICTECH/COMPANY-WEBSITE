ab97ca935645513116dc72a3a413021c3615398d
Uyiosa@SoftDev <88433898+Uyiosa2607@users.noreply.github.com>
2026-06-06 09:51:29 +0100

    updated styling and layout for mobile

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
     <>
       <Header />
       <main className="py-8 px-3 bg-slate-50">
-        <div className="max-w-4xl mt-4  mx-auto bg-white rounded-xl shadow p-6">
+        <div className="max-w-4xl mt-4 p-2.5 mx-auto bg-white rounded-xl shadow md:p-6">
           <h1 className="text-xl lg:text-2xl font-bold mb-4 text-blue-600">
             <span className="text-indigo-600">
               <span className="text-gray-700">Project Cost Estimator</span>
@@ -54,7 +54,7 @@ export default function Home() {
               {[1, 2, 3, 4].map((i) => (
                 <div
                   key={i}
-                  className="border border-slate-200 rounded-xl p-5 space-y-3"
+                  className="border border-slate-200 rounded-xl p-2 md:p-5 space-y-3"
                 >
                   <div className="h-4 bg-slate-200 rounded w-1/4" />
                   <div className="h-3 bg-slate-100 rounded w-1/3" />
